78bd7d7d49ce7969b5855c45b7f79e79f37d1d45
LucasOli82 <lucasomelo701@gmail.com>
2025-08-31 12:56:25 -0300

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://github.com/LucasOli82/js-developer-portfolio';
+    const url = 'https://raw.githubusercontent.com/LucasOli82/js-developer-portfolio/refs/heads/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
